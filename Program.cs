e019e7441aea5d966b03aba7760e6cdcd4bc7d43
Fernic888 <122941312+Fernic888@users.noreply.github.com>
2023-02-07 08:30:31 -0300

    Cracion de clases mapeando la bd y sus correspondientes manejadores

--- Program.cs
+++ Program.cs
@@ -1,10 +1,20 @@
-﻿namespace PrimerEntrega
+﻿using System.Runtime.Intrinsics.X86;
+using System;
+
+namespace PrimerEntrega
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+        //Traer Usuario(recibe un int)
+
+
+        //Traer Productos(recibe un id de usuario y, devuelve una lista con todos los productos cargado por ese usuario)
+        //Traer ProductosVendidos(recibe el id del usuario y devuelve una lista de productos vendidos por ese usuario)
+        //Traer Ventas(recibe el id del usuario y devuelve un a lista de Ventas realizadas por ese usuario)
+        //Inicio de sesión(recibe un usuario y contraseña y devuelve un objeto Usuario)
+
         }
     }
 }
